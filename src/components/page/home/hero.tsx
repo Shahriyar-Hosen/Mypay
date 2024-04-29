f50f670254e21f19f4bbf32701eef176811c5e48
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-04-29 23:19:43 +0600

    init done

--- src/components/page/home/hero.tsx
+++ src/components/page/home/hero.tsx
@@ -1,7 +1,21 @@
 export const Hero = () => {
   return (
     <section>
-      <div>text</div>
+      <div>
+        <h1 className="max-w-[711px] font-medium text-5xl ">
+          <span className="bg-gradient-to-r from-[#4B86FC] to-[#75F2F6] bg-clip-text text-transparent font-bold">
+            MyPay
+          </span>
+          &nbsp;Plateforme de gestion de paiements en ligne pour business
+          High-Risk.
+        </h1>
+        <p className="text-sm font-medium max-w-[557px] text-[#E1E1E1] leading-6">
+          Business High-Risk pris en charge : Servitia Financia et Investitiones
+          , Mercator es titulorum Pecuniae mutuatae ad diem Mercatores cryptae
+          valutarum. Servitia Financia et Investitiones , Mercator es titulorum
+          Pecuniae mutuatae ad diem Mercatores cryptae valutarum.
+        </p>
+      </div>
       <div>img</div>
     </section>
   );
