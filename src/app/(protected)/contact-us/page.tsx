89f5787e304d71cc747bb6c8bb35652544ddf2ab
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-05-01 00:05:12 +0600

    Contact us page setup done

--- src/app/(protected)/contact-us/page.tsx
+++ src/app/(protected)/contact-us/page.tsx
@@ -1,5 +1,10 @@
 const ContactUs = () => {
-  return <main className="min-h-screen">ContactUs</main>;
+  return (
+    <main className="min-h-screen">
+      <h1>Contact us</h1>
+      <section>Contact us Form</section>
+    </main>
+  );
 };
 
 export default ContactUs;
