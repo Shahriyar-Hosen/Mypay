bc61f15fae9c1900543e68e1cf967780493927ff
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-05-01 23:47:26 +0600

    hover effect added

--- src/components/page/home/subscribe/card-container.tsx
+++ src/components/page/home/subscribe/card-container.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 
 export const CardContainer: FC<IChildren> = ({ children }) => (
-  <div className="p-3.5 md:p-6 flex flex-col justify-start items-center gap-5 sm:gap-8 md:gap-10 lg:gap-12">
+  <div className="p-3.5 md:p-6 flex flex-col justify-start items-center gap-5 sm:gap-8 md:gap-10 lg:gap-12 relative z-30">
     {children}
   </div>
 );
