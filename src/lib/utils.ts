cf5e8a1775ee931c75a041bbc19ad8fb6d18a93a
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-05-01 14:49:23 +0600

    all done in

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,11 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
+/**
+ * A utility function that combines multiple class names into a single string,
+ * using `clsx` to concatenate and `tailwind-merge` to ensure correct Tailwind CSS merging.
+ *
+ * @param {...ClassValue[]} inputs - A list of class name inputs to be combined.
+ * @returns {string} - A single string of combined class names, merged for Tailwind CSS.
+ */
+export const cn = (...inputs: ClassValue[]): string => twMerge(clsx(inputs));
